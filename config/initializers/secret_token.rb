5a3ffc022985be5654abf05b832d56eba6df88d2
dokoresol <do@koresol.com>
2014-05-09 08:55:14 -0700

    Initial rails install

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dotest::Application.config.secret_token = 'fa28a0024501a91fd255321ecac6b34a8768ca8226a8407485a81a09563cabd8c920deaf4a2e8996d4d0cac532e458f38823f0ae9a129136f026d08afe0c34e0'
